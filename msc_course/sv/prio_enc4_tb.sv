fef3cf0e866375af3009dcbc7bdac92b82449d5c
Kishan Shanthakumar (ks8n22) <ks8n22@soton.ac.uk>
2022-10-18 09:52:42 +0100

    Incomplete mock assignment

--- msc_course/sv/prio_enc4_tb.sv
+++ msc_course/sv/prio_enc4_tb.sv
@@ -2,18 +2,14 @@ module prio_enc_tb();
 
 logic [3:0] a;
 logic [1:0] b;
+logic valid;
 
 initial begin
-a = 8;
-#10 a = 4'hc;
-#10 a = 4'he;
-#10 a = 4'hf;
-#10 a = 7;
-#10 a = 3;
-#10 a = 1;
-#10 ;
+a = 0;
 end
 
-//prio_enc u1(a,b);
+always #10 a += 1;
+
+enc_n #(2) u1(b, valid, a);
 
 endmodule
