16f0a2972ac1319c29bdebc274921df37bced464
Kishan S <kishan.shanthakumar@gmail.com>
2022-10-28 03:04:26 +0100

    fmul added mul modules

--- sv/fmul.sv
+++ sv/fmul.sv
@@ -9,28 +9,7 @@ Steps in multiplication:
 1. Add the exponents and subtract 127
 2. Multiply the mantissa
 3. Check the sign of the number
-4. Adjust shifting of mantissa
 */
-
-
-module enc_n #(parameter N = 5)
-					(output logic [N-1:0] out,
-					output logic valid,
-					input logic [2**N-1:0] inp);
-
-always_comb
-begin
-	valid = 0;
-	for (int i = 2**N-1; i >= 0; i--)
-		if (inp[i] == 1)
-		begin
-			out = i;
-			valid = 1;
-			break;
-		end
-end
-
-endmodule
 
 module fmul #(parameter N = 32)
             (input logic [N-1:0] a, b,
@@ -49,9 +28,15 @@ module fmul #(parameter N = 32)
     parameter enc_len = 5;
 `endif
 
-reg [N-1:0] ff11, ff12;
-reg [N-1:0] ffa, ffb;
-reg [N-1:0] ff21, ff22;
-reg [N-1:0] ff3;
+logic [N-1:0] exp_calc;
+logic [N-1:0] exp_calc1;
+cseladd #(exp_len) u1(a[exp:man+1], b[exp:man+1], 0, exp_calc);
+cseladd #(exp_len) u1(exp_calc, ~(2**(exp_len-1)-1), 1, exp_calc1);
+
+always_comb @(posedge clk, negedge rst)
+begin
+    out[N-1] = a[N-1] ^ b[N-1];
+    
+end
 
 endmodule
