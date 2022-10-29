4f0ac647aa127afafe410a8173d326b9bf3b47a9
Kishan S <kishan.shanthakumar@gmail.com>
2022-10-29 04:44:07 +0100

    TODO: exp calc verification

--- sv/nmul.sv
+++ sv/nmul.sv
@@ -2,13 +2,16 @@ module nmul #(parameter N = 32)
             (input logic [N-1:0] a,b,
             output logic [N*2-1:0] mul);
 
+logic [N*2-1:0] temp;
+
 always_comb
 begin
-    mul = 0;
+    temp = 0;
     for(int i = 0; i < N; i++)
     begin
-        mul += b[i]? a<<i : '0;
+        temp += b[i]? a<<i : 0;
     end
+    mul = temp;
 end
 
 endmodule
