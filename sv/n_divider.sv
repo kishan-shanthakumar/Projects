43c0c4c5cf66917fbbc13cb3ab6e1ccdd3a3ef47
Kishan S <kishan.shanthakumar@gmail.com>
2022-10-30 01:34:44 +0000

    Modified divider

--- sv/n_divider.sv
+++ sv/n_divider.sv
@@ -3,24 +3,15 @@ module n_divider #(parameter N = 32)
             output logic [N*2-1:0] div);
 
 logic [N*2-1:0] x;
-logic [N*2-1:0] dx;
-logic [N*2-1:0] dx2;
-logic [N*2-1:0] x2dx;
-logic [N*2-1:0] temp;
-
-nmul #(N) u1(x[N*2-1-:N],b,dx);
-nmul #(N) u2(dx2[N*2-1-:N],x[N*2-1-:N],x2dx);
-nmul #(N) u3(a,x[N*2-1-:N],temp);
 
 always_comb
 begin
-    x = 0;
+    x = 1;
     for(int i = 0; i < 10; i++)
     begin
-        dx2 = 2-dx[N*2-1-:N];
-        x = x2dx;
+        x = x*(2-b*x);
     end
-    div = temp;
+    div = x * a;
 end
 
 endmodule
