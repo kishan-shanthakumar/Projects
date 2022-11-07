2a76fbdd210dc6442f7fc242aa34f0ef2206fc1d
Kishan S <kishan.shanthakumar@gmail.com>
2022-11-07 02:34:27 +0000

    Seperated out enc_n module

--- sv/fadd.sv
+++ sv/fadd.sv
@@ -18,25 +18,6 @@ Stage 2: Using shifted valued if necessary
 Stage 3: Calulating final value
 */
 
-module enc_n #(parameter N = 5)
-					(output logic [N-1:0] out,
-					output logic valid,
-					input logic [2**N-1:0] inp);
-
-always_comb
-begin
-	valid = 0;
-	for (int i = 2**N-1; i >= 0; i--)
-		if (inp[i] == 1)
-		begin
-			out = i;
-			valid = 1;
-			break;
-		end
-end
-
-endmodule
-
 module fadd #(parameter N = 32)
             (input logic [N-1:0] a, b,
             output logic [N-1:0] out);
