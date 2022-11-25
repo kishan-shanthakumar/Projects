a93f34de787990da205b78577ce31bf8668f4fe6
Kishan S <kishan.shanthakumar@gmail.com>
2022-11-25 03:46:49 +0000

    Added special cases support

--- sv/sv_assignment_tb.sv
+++ sv/sv_assignment_tb.sv
@@ -27,16 +27,9 @@ rb = $shortrealtobits(realb);
 a = ra[31:16];
 b = rb[31:16];
 @(posedge ready);
-@(posedge clock);
 realsum = $bitstoshortreal({sum, {16{1'b0}}});
 $display("Test 2 %f\n", realsum);
 
-nreset = '1;
-#5ns nreset = '1;
-#5ns nreset = '0;
-#5ns nreset = '1;
-@(posedge ready); // wait for ready
-//Test 2 -- 1.0 + 1.0
 reala = 1.8;
 ra = $shortrealtobits(reala);
 realb = 2.6;
@@ -44,16 +37,9 @@ rb = $shortrealtobits(realb);
 a = ra[31:16];
 b = rb[31:16];
 @(posedge ready);
-@(posedge clock);
 realsum = $bitstoshortreal({sum, {16{1'b0}}});
 $display("Test 2 %f\n", realsum);
 
-nreset = '1;
-#5ns nreset = '1;
-#5ns nreset = '0;
-#5ns nreset = '1;
-@(posedge ready); // wait for ready
-//Test 2 -- 1.0 + 1.0
 reala = 1.0;
 ra = $shortrealtobits(reala);
 realb = 0.0;
@@ -61,16 +47,9 @@ rb = $shortrealtobits(realb);
 a = ra[31:16];
 b = rb[31:16];
 @(posedge ready);
-@(posedge clock);
 realsum = $bitstoshortreal({sum, {16{1'b0}}});
 $display("Test 2 %f\n", realsum);
 
-nreset = '1;
-#5ns nreset = '1;
-#5ns nreset = '0;
-#5ns nreset = '1;
-@(posedge ready); // wait for ready
-//Test 2 -- 1.0 + 1.0
 reala = -1.0;
 ra = $shortrealtobits(reala);
 realb = 0.0;
@@ -78,16 +57,9 @@ rb = $shortrealtobits(realb);
 a = ra[31:16];
 b = rb[31:16];
 @(posedge ready);
-@(posedge clock);
 realsum = $bitstoshortreal({sum, {16{1'b0}}});
 $display("Test 2 %f\n", realsum);
 
-nreset = '1;
-#5ns nreset = '1;
-#5ns nreset = '0;
-#5ns nreset = '1;
-@(posedge ready); // wait for ready
-//Test 2 -- 1.0 + 1.0
 reala = -1.0;
 ra = $shortrealtobits(reala);
 realb = 1.0;
@@ -95,16 +67,9 @@ rb = $shortrealtobits(realb);
 a = ra[31:16];
 b = rb[31:16];
 @(posedge ready);
-@(posedge clock);
 realsum = $bitstoshortreal({sum, {16{1'b0}}});
 $display("Test 2 %f\n", realsum);
 
-nreset = '1;
-#5ns nreset = '1;
-#5ns nreset = '0;
-#5ns nreset = '1;
-@(posedge ready); // wait for ready
-//Test 2 -- 1.0 + 1.0
 reala = 1.0;
 ra = $shortrealtobits(reala);
 realb = -1.0;
@@ -112,8 +77,68 @@ rb = $shortrealtobits(realb);
 a = ra[31:16];
 b = rb[31:16];
 @(posedge ready);
-@(posedge clock);
 realsum = $bitstoshortreal({sum, {16{1'b0}}});
 $display("Test 2 %f\n", realsum);
+
+reala = 0;
+ra = 32'hffffffff;
+realb = -1.0;
+rb = $shortrealtobits(realb);
+a = ra[31:16];
+b = rb[31:16];
+@(posedge ready);
+realsum = $bitstoshortreal({sum, {16{1'b0}}});
+$display("Test 2 %f\n", realsum);
+
+reala = 0.0;
+ra = $shortrealtobits(reala);
+realb = 0.0;
+rb = $shortrealtobits(realb);
+a = ra[31:16];
+b = rb[31:16];
+@(posedge ready);
+realsum = $bitstoshortreal({sum, {16{1'b0}}});
+$display("Test 2 %f\n", realsum);
+
+reala = 1;
+ra = 32'hff800000;
+realb = -1.0;
+rb = $shortrealtobits(realb);
+a = ra[31:16];
+b = rb[31:16];
+@(posedge ready);
+realsum = $bitstoshortreal({sum, {16{1'b0}}});
+$display("Test 2 %f\n", realsum);
+
+reala = 0.0;
+ra = 32'h80000000;
+realb = -0.0;
+rb = 32'h00000000;
+a = ra[31:16];
+b = rb[31:16];
+@(posedge ready);
+realsum = $bitstoshortreal({sum, {16{1'b0}}});
+$display("Test 2 %f\n", realsum);
+
+reala = 0.0;
+ra = 32'h00000000;
+realb = -0.0;
+rb = 32'h80000000;
+a = ra[31:16];
+b = rb[31:16];
+@(posedge ready);
+realsum = $bitstoshortreal({sum, {16{1'b0}}});
+$display("Test 2 %f\n", realsum);
+
+reala = 0.0;
+ra = 32'h80000000;
+realb = -0.0;
+rb = 32'h80000000;
+a = ra[31:16];
+b = rb[31:16];
+@(posedge ready);
+realsum = $bitstoshortreal({sum, {16{1'b0}}});
+$display("Test 2 %f\n", realsum);
+
 end
 endmodule
