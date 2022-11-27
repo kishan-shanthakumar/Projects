c6e935c2dec7e00c49b9003e8124fd099f512ead
Kishan S <kishan.shanthakumar@gmail.com>
2022-11-27 11:42:18 +0000

    Handling of warnings

--- sv/bfloat16_adder.sv
+++ sv/bfloat16_adder.sv
@@ -18,17 +18,24 @@ Stage 2: Using shifted valued if necessary
 Stage 3: Calulating final value
 */
 
-module enc_n #(parameter N = 5)
-					(output logic [N-1:0] out,
-					input logic [2**N-1:0] inp);
+function [22:0] trunc_24_to_23(input [23:0] val32);
+  trunc_24_to_23 = val32[22:0];
+endfunction
+
+function [4:0] trunc_32_to_5(input [31:0] val32);
+  trunc_32_to_5 = val32[4:0];
+endfunction
+
+module enc_n (output logic [4:0] out,
+                input logic [22:0] inp);
 
 always_comb
 begin
-	out = 0;
-	for (int i = 2**N-1; i >= 0; i--)
+	out = 5'b0;
+	for (int i = 22; i >= 0; i--)
 		if (inp[i] == 1)
 		begin
-			out = i;
+			out = trunc_32_to_5(i);
 			break;
 		end
 end
@@ -37,7 +44,7 @@ endmodule
 
 module cseladd #(parameter N = 32)
 		(input logic [N-1:0] a,b,
-		input logic cin,
+        input cin,
 		output logic [N:0] out);
 
 logic [N-1:0] carry1;
@@ -54,7 +61,7 @@ begin
     begin
         sum0[i] = a[i] ^ b[i];
         carry0[i] = a[i] & b[i];
-        sum1[i] = a[i] ^ b[i] ^ 1;
+        sum1[i] = a[i] ^ b[i] ^ 1'b1;
         carry1[i] = (a[i] & b[i]) + (a[i] ^ b[i]);
     end
 
@@ -156,11 +163,11 @@ logic pass0;
 
 cseladd #(exp_len) u1(a[exp:man+1],~b[exp:man+1],1,shft_amtab);
 cseladd #(exp_len) u2(b[exp:man+1],~a[exp:man+1],1,shft_amtba);
-cseladd #(man+1) u4({1'b1,ff21[man:0]},{flag1,~ff22[man:0]},1,outab);
-cseladd #(man+1) u5({1'b1,ff22[man:0]},{flag1,~ff21[man:0]},1,outba);
-enc_n #(enc_len) u6(outcalcab,outab[man:0]);
-enc_n #(enc_len) u7(outcalcba,outba[man:0]);
-cseladd #(man+1) u3(ff21[man:0],ff22[man:0],0,wi);
+cseladd #(man+2) u4({1'b1,ff21[man:0]},{flag1,~ff22[man:0]},1,outab);
+cseladd #(man+2) u5({1'b1,ff22[man:0]},{flag1,~ff21[man:0]},1,outba);
+enc_n u6(outcalcab,outab[man:0]);
+enc_n u7(outcalcba,outba[man:0]);
+cseladd #(man+2) u3(ff21[man:0],ff22[man:0],0,wi);
 
 assign ready = ready_st[2];
 assign sum = ff3;
@@ -182,9 +189,9 @@ begin
 		if (pass == 0)
 		begin
 			ff11[N-1:man+1] <= {a[N-1], b[exp:man+1]};
-			ff11[man:0] <= {1'b1,a[man:0]}>>shft_amtba;
+			ff11[man:0] <= trunc_24_to_23({1'b1,a[man:0]}>>shft_amtba);
 			ff12[N-1:man+1] <= {b[N-1], a[exp:man+1]};
-			ff12[man:0] <= {1'b1,b[man:0]}>>shft_amtab;
+			ff12[man:0] <= trunc_24_to_23({1'b1,b[man:0]}>>shft_amtab);
 			ffa <= a;
 			ffb <= b;
 		end
@@ -291,7 +298,7 @@ begin
             if (ff21[N-1] == ff22[N-1])
             begin
                 ff3[N-1] <= ff21[N-1];
-                ff3[exp:man+1] <= ff21[exp:man+1] + 1;
+                ff3[exp:man+1] <= ff21[exp:man+1] + 1'b1;
                 ff3[man:0] <= ff21[man:0];
             end
             else
@@ -312,14 +319,14 @@ begin
                     if (ff21[man:0]>ff22[man:0])
                     begin
                         ff3[N-1] <= ff21[N-1];
-                        ff3[N-2:man+1] <= ff21[N-2:man+1] - (2**enc_len-outcalcab-9);
-                        ff3[man:0] <= outab[man:0]<<(2**enc_len-outcalcab-9);
+                        ff3[N-2:man+1] <= ff21[N-2:man+1] - outcalcab;
+                        ff3[man:0] <= outab[man:0]<<outcalcab;
                     end
                     else
                     begin
                         ff3[N-1] <= ff22[N-1];
-                        ff3[N-2:man+1] <= ff21[N-2:man+1] - (2**enc_len-outcalcba-9);
-                        ff3[man:0] <= outba[man:0]<<(2**enc_len-outcalcba-9);
+                        ff3[N-2:man+1] <= ff21[N-2:man+1] - outcalcba;
+                        ff3[man:0] <= outba[man:0]<<outcalcba;
                     end
                 else
                     if (flag)
