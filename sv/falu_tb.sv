efeb9f5220a443302193f71eeba3490a3843676e
Kishan S <kishan.shanthakumar@gmail.com>
2022-12-05 15:21:51 +0000

    Changes to accomodate checking

--- sv/falu_tb.sv
+++ sv/falu_tb.sv
@@ -8,23 +8,23 @@ rand bit sign_rand;
 endclass
 
 module falu_tb;
-logic [15:0] out;
-logic [15:0] a, b;
+logic [31:0] out;
+logic [31:0] a, b;
 logic [1:0] s;
 shortreal reala, realb;
 shortreal realans;
 real realout;
 logic [31:0] ra, rb;
 int check;
-falu a1 (.*);
+falu #(32) a1 (.*);
 initial
 begin
     packet pkt1;
     for(int i = 0; i < 256; i += 1)
     begin
-        #5
         pkt1 = new();
         check = pkt1.randomize();
+        s = pkt1.s_rand;
         ra = {pkt1.sign_rand, pkt1.exp_rand, pkt1.man_rand};
         reala = $bitstoshortreal(ra);
         $display("a is %f", reala);
@@ -37,10 +37,12 @@ begin
         $display("b is %f", realb);
         b = rb;
 
-        #5
-        realout = $bitstoshortreal({out, {16{1'b0}}});
+        #1;
+        realout = $bitstoshortreal(out);
         realans = (s == 2'b00) ? reala+realb : ( (s == 2'b01) ? reala-realb : ( (s == 2'b10) ? reala*realb : reala/realb) );
         $display("Error %f", (((realout-realans)/realans)*100));
+        
+        #5;
     end
 end
 endmodule
