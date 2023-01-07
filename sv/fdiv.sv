b11f6781c81421dfefa0cca2aae64a0b7dd433f7
Kishan S <kishan.shanthakumar@gmail.com>
2023-01-07 04:04:09 +0000

    Special cases

--- sv/fdiv.sv
+++ sv/fdiv.sv
@@ -27,20 +27,40 @@ module fdiv #(parameter N = 32)
     parameter enc_len = 5;
 `endif
 
-logic [N-1:0] exp_calc;
-logic [N-1:0] exp_calc1;
+logic [exp_len-1:0] exp_calc;
+logic [exp_len-1:0] exp_calc1;
 logic [man+1:0] div;
 logic temp;
 logic [man+1:0] temp_man;
-logic [enc_len:0] shft;
+logic [enc_len-1:0] shft;
 
 cseladd #(exp_len) u1(a[exp:man+1], (2**(exp_len-1)-1), 0, exp_calc);
 cseladd #(exp_len) u2(exp_calc, ~b[exp:man+1], 1, exp_calc1);
 n_divider #((man+2)*2) u3({1,a[man:0],{(man+2){1'b0}}},{1,b[man:0]},div,temp_man);
 enc_n #(enc_len) u4(shft,temp,div);
 
-assign out[N-1] = a[N-1] ^ b[N-1];
-assign out [exp:man+1] = exp_calc1 - (man + 2 - shft);
-assign out [man:0] = div[man:0];
+always_comb
+begin
+    out[N-1] = a[N-1] ^ b[N-1];
+    if (a[exp:man+1] == '1 | b[exp:man+1] == '1)
+        if (a[man:0] > 0 | b[man:0] > 0)
+            out[exp:0] = '1;
+        else
+            out[exp:0] = {{exp{1'b1}},{man{1'b0}}};
+    else if (a[exp:0] == 0| b[exp:0] == 0)
+        out[exp:0] = (a[exp:0] == 0) ? 0 : {N{1'b1}};
+    else if ($signed(a[exp:man+1]-b[exp:man+1]) > $signed((2**(exp_len-1)-1)))
+    begin
+        out[exp:man+1] = '1;
+        out[man:0] = 0;
+    end
+    else if ($signed(a[exp:man+1]-b[exp:man+1]) < $signed(-(2**(exp_len-1)-2)))
+        out[exp:0] = 0;
+    else
+    begin
+        out [exp:man+1] = exp_calc1 - (man + 2 - shft);
+        out [man:0] = div[man:0] << (man + 2 - shft);;
+    end
+end
 
 endmodule
