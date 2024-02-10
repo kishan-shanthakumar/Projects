d201e16dd070a3c794bff7051c3baefad9a0454d
Kishan S <kishan.shanthakumar@gmail.com>
2024-02-10 00:44:51 +0000

    removed print

--- skydive_data_logger/computer_files/main.py
+++ skydive_data_logger/computer_files/main.py
@@ -28,7 +28,7 @@ try:
     mpu = MPU6050(MPU_addr, bus)
     mpu.MPU_Init()
 except OSError:
-    print('MPU6050 not found, skipping MPU6050 operations')
+    # print('MPU6050 not found, skipping MPU6050 operations')
     mpu_flag = 0
 
 # DPS310 initialisation
@@ -36,7 +36,7 @@ dps_flag = 1
 try:
     dps310 = DPS(DPS_addr, bus)
 except OSError:
-    print('DPS310 not found, skipping DPS310 operations')
+    # print('DPS310 not found, skipping DPS310 operations')
     dps_flag = 0
 
 # MCP9808 initiaslisation
@@ -45,7 +45,7 @@ try:
     mcp = MCP(MCP_addr, bus)
     mcp.MCP_Init()
 except OSError:
-    print('MCP9808 not found, skipping MCP9808 operations')
+    # print('MCP9808 not found, skipping MCP9808 operations')
     mcp_flag = 0
 
 # GPS initialisation
@@ -79,27 +79,27 @@ try:
             di['gx'].append(mpu_val[3])
             di['gy'].append(mpu_val[4])
             di['gz'].append(mpu_val[5])
-            print(mpu_val)
+            # print(mpu_val)
 
         if dps_flag == 1:
             scaled_p = dps310.calcScaledPressure()
             scaled_t = dps310.calcScaledTemperature()
             p = dps310.calcCompPressure(scaled_p, scaled_t)
             t = dps310.calcCompTemperature(scaled_t)
-            print((10**((log(p/101325)/log(2.718))/5.2558797)-1/(-6.8755856*10**-6) ) , 'ft')
-            print(t,'C')
+            # print((10**((log(p/101325)/log(2.718))/5.2558797)-1/(-6.8755856*10**-6) ) , 'ft')
+            # print(t,'C')
             di['pr'].append(p)
             di['te'].append(t)
 
         if mcp_flag == 1:
             extern_temp = mcp.mcp_func()
-            print(extern_temp)
+            # print(extern_temp)
             di['et'].append(extern_temp)
         
         time.sleep(0.1)
 
     # gps_val = gps.gps_run()
-    # print(gps_val)
+    # # print(gps_val)
 except KeyboardInterrupt:
     plt.subplot(2,2,1)
     plt.plot(di['time'], di['ax'])
