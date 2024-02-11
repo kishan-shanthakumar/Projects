4c0786d29330102e1672e792d98a1eada9bbdfbc
Kishan S <kishan.shanthakumar@gmail.com>
2024-02-11 07:44:40 +0000

    changed reading format

--- skydive_data_logger/computer_files/gps.py
+++ skydive_data_logger/computer_files/gps.py
@@ -8,17 +8,17 @@ class GPS:
     def gps_data(self, unused):
         while True:
             ser_data = self.ser.readline()
-            if ser_data.split(',')[2] == "A":
+            if ser_data.split(',')[0] == '$GPGGA' and int(ser_data.split(',')[6]) > 0:
                 self.received_data = ser_data
 
     def gps_run(self):
         data = self.received_data.split()
         if len(data) > 2:
             di = {}
-            di['time'] = data[1] + data[8]
-            di['lat'] = data[3] + data[4]
-            di['lon'] = data[5] + data[6]
-            di['speed'] = data[7]
+            di['time'] = data[1]
+            di['lat'] = data[2] + data[3]
+            di['lon'] = data[4] + data[5]
+            di['alt'] = data[9]
             return(di)
         else:
             return 0
