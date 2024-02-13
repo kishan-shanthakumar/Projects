6973d4b61f20e3d5762c6dae3cd6cb8c5a4c64fe
Kishan S <kishan.shanthakumar@gmail.com>
2024-02-13 04:51:56 +0000

    add xscroll

--- skydive_data_logger/computer_files/start_interface.py
+++ skydive_data_logger/computer_files/start_interface.py
@@ -35,8 +35,16 @@ canvas.pack(side="left", fill="both", expand=1)
 my_scrollbar = tk.Scrollbar(main_frame, orient="vertical", command=canvas.yview)
 my_scrollbar.pack(side="right", fill="y")
 
+mx_scrollbar = tk.Scrollbar(main_frame, orient="horizontal", command=canvas.xview)
+mx_scrollbar.pack(side="bottom", fill="x")
+
 # configure the canvas
 canvas.configure(yscrollcommand=my_scrollbar.set)
+canvas.bind(
+    '<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+)
+
+canvas.configure(xscrollcommand=mx_scrollbar.set)
 canvas.bind(
     '<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
 )
