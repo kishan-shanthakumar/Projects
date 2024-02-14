b98892edd12ef0a7ecaba1508fa3f57dff8f5cfe
Kishan S <kishan.shanthakumar@gmail.com>
2024-02-14 21:00:41 +0000

    buffer current di

--- skydive_data_logger/computer_files/sensor_read.py
+++ skydive_data_logger/computer_files/sensor_read.py
@@ -58,19 +58,20 @@ class Sensors:
     def sensor_read(self, unused):
         while True:
             ts = time.time() - self.ti
-            self.di['mpu6050'] = {}
-            self.di['dps'] = {}
-            self.di['gps'] = {}
-            self.di['time'] = (ts)
+            di_temp = {}
+            di_temp['mpu6050'] = {}
+            di_temp['dps'] = {}
+            di_temp['gps'] = {}
+            di_temp['time'] = (ts)
 
             if self.mpu_flag == 1:
                 mpu_val = self.mpu.mpu_read()
-                self.di['mpu6050']['ax'] = (mpu_val[0])
-                self.di['mpu6050']['ay'] = (mpu_val[1])
-                self.di['mpu6050']['az'] = (mpu_val[2])
-                self.di['mpu6050']['gx'] = (mpu_val[3])
-                self.di['mpu6050']['gy'] = (mpu_val[4])
-                self.di['mpu6050']['gz'] = (mpu_val[5])
+                di_temp['mpu6050']['ax'] = (mpu_val[0])
+                di_temp['mpu6050']['ay'] = (mpu_val[1])
+                di_temp['mpu6050']['az'] = (mpu_val[2])
+                di_temp['mpu6050']['gx'] = (mpu_val[3])
+                di_temp['mpu6050']['gy'] = (mpu_val[4])
+                di_temp['mpu6050']['gz'] = (mpu_val[5])
                 # print(mpu_val)
 
             if self.dps_flag == 1:
@@ -80,22 +81,22 @@ class Sensors:
                 t = self.dps310.calcCompTemperature(scaled_t)
                 # print((10**((log(p/101325)/log(2.718))/5.2558797)-1/(-6.8755856*10**-6) ) , 'ft')
                 # print(t,'C')
-                self.di['dps']['pr'] = (p)
-                self.di['dps']['te'] = (t)
+                di_temp['dps']['pr'] = (p)
+                di_temp['dps']['te'] = (t)
 
             if self.mcp_flag == 1:
                 extern_temp = self.mcp.mcp_func()
                 # print(extern_temp)
-                self.di['et'] = (extern_temp)
+                di_temp['et'] = (extern_temp)
             
             gps_val = self.gps.gps_run()
             if not gps_val == 0:
                 # print(gps_val)
-                self.di['gps']['time'] = (gps_val['time'])
-                self.di['gps']['lat'] = (gps_val['lat'])
-                self.di['gps']['lon'] = (gps_val['lon'])
-                self.di['gps']['alt'] = (gps_val['alt'])
-            print(self.di)
+                di_temp['gps']['time'] = (gps_val['time'])
+                di_temp['gps']['lat'] = (gps_val['lat'])
+                di_temp['gps']['lon'] = (gps_val['lon'])
+                di_temp['gps']['alt'] = (gps_val['alt'])
+            self.di = di_temp
     
     def values(self):
         return self.di
